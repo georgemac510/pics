376dc616b25d66388bc5ad5ee37b7da3f697b04b
georgemac510 <33005672+georgemac510@users.noreply.github.com>
2019-02-22 22:07:17 -0500

    Add existing files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization:
+            'Client-ID 8a23f49ff9fef406417812330745c3bd0fc50c48f95fdad989a68a59b776b746'
+      }
+    });
+  console.log(response.data.results);
+}
 
   render() {
   return (
